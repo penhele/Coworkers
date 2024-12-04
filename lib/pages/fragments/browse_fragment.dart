9734296d92b9fe8bb948dabfb2a0a14a06f077a1
penhele <stephenhelk@gmail.com>
2024-12-04 20:35:17 +0700

    Browse Controller

--- lib/pages/fragments/browse_fragment.dart
+++ lib/pages/fragments/browse_fragment.dart
@@ -1,7 +1,22 @@
+import 'package:coworkers/controllers/fragments/browse_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class BrowseFragment extends StatelessWidget {
+class BrowseFragment extends StatefulWidget {
   const BrowseFragment({super.key});
+
+  @override
+  State<BrowseFragment> createState() => _BrowseFragmentState();
+}
+
+class _BrowseFragmentState extends State<BrowseFragment> {
+  final browseController = Get.put(BrowseController());
+
+  @override
+  void dispose() {
+    browseController.clear();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
