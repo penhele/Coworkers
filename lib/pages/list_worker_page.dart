5137b7c40d146806fac938ca57a364d721a37b33
penhele <stephenhelk@gmail.com>
2024-12-10 15:18:09 +0700

    Search Box

--- lib/pages/list_worker_page.dart
+++ lib/pages/list_worker_page.dart
@@ -1,7 +1,6 @@
 import 'package:coworkers/config/app_color.dart';
 import 'package:coworkers/controllers/list_worker_controller.dart';
 import 'package:coworkers/widgets/header_worker.dart';
-import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -45,25 +44,78 @@ class _ListWorkerPageState extends State<ListWorkerPage> {
                         bottomRight: Radius.circular(80),
                       )),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 40),
-                      child: HeaderWorker(
-                        title: widget.category,
-                        subtitle: '13,492 workers',
-                        iconLeft: 'assets/ic_back.png',
-                        functionLeft: () => Navigator.pop(context),
-                        iconRight: 'assets/ic_filter.png',
-                        functionRight: () => {}
+                Transform.translate(
+                  offset: const Offset(0, 25),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40),
+                        child: HeaderWorker(
+                          title: widget.category,
+                          subtitle: '13,492 workers',
+                          iconLeft: 'assets/ic_back.png',
+                          functionLeft: () => Navigator.pop(context),
+                          iconRight: 'assets/ic_filter.png',
+                          functionRight: () => {}
+                        ),
                       ),
-                    ),
-                    DView.nothing(),
-                  ],
+                      searchBox(),
+                    ],
+                  ),
                 )
               ],
             ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      height: 50,
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: const Color(0xffE5E7EC).withOpacity(0.5),
+            blurRadius: 30,
+            offset: const Offset(0, 6)
+          )
+        ]
+      ),
+
+      alignment: Alignment.bottomLeft,
+      padding: const EdgeInsets.only(left: 20, right: 8),
+
+      child: Row(
+        children: [
+          const Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search by name',
+                hintStyle: TextStyle(
+                  color: Color(0xffA7A8B3),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.all(0),
+                isDense: true
+              ),
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const ImageIcon(AssetImage('assets/ic_search.png')),
           )
         ],
       ),
