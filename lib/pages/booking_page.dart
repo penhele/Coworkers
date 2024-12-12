c65b63f475fea9a24bcba68e44b399736fc62c84
penhele <stephenhelk@gmail.com>
2024-12-12 23:15:04 +0700

    Profile Section

--- lib/pages/booking_page.dart
+++ lib/pages/booking_page.dart
@@ -1,4 +1,6 @@
 import 'package:coworkers/config/app_color.dart';
+import 'package:coworkers/config/app_format.dart';
+import 'package:coworkers/config/appwrite.dart';
 import 'package:coworkers/controllers/booking_controller.dart';
 import 'package:coworkers/controllers/user_controller.dart';
 import 'package:coworkers/models/worker_model.dart';
@@ -48,25 +50,108 @@ class _BookingPageState extends State<BookingPage> {
                         bottomRight: Radius.circular(80),
                       )),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    HeaderWorkerLeft(
-                      title: "Booking Worker",
-                      subtitle: 'Grow your bussiness today',
-                      iconLeft: 'assets/ic_back.png',
-                      functionLeft: () {
-                        Navigator.pop(context);
-                      }
-                    ),
-                    DView.empty(),
-
-                  ],
+                Transform.translate(
+                  offset: Offset(0, 60),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HeaderWorkerLeft(
+                        title: "Booking Worker",
+                        subtitle: 'Grow your bussiness today',
+                        iconLeft: 'assets/ic_back.png',
+                        functionLeft: () {
+                          Navigator.pop(context);
+                        }
+                      ),
+                      worker(),
+                    ],
+                  ),
                 ),
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+  
+  Widget worker() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Material(
+        color: Colors.white,
+        elevation: 8,
+        shadowColor: Colors.black12,
+        borderRadius: BorderRadius.circular(16),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Image.network(
+                AppWrite.imageURL(widget.worker.image),
+                width: 70,
+                height: 70,
+              ),
+              DView.width(12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          widget.worker.name,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black
+                          ),
+                        ),
+                        DView.width(4),
+                        Image.asset(
+                          'assets/ic_verified.png',
+                          width: 16,
+                          height: 16,
+                        )
+                      ],
+                    ),
+                    DView.height(2),
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/ic_star_small.png',
+                          width: 16,
+                          height: 16,
+                        ),
+                        DView.height(4),
+                        Text(
+                          widget.worker.rating.toString(),
+                          style: const TextStyle(
+                            color: Colors.black
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              DView.width(12),
+              Row(
+                children: [
+                  Text(
+                    AppFormat.price(widget.worker.hourRate),
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                    ),
+                  ),
+                  const Text('/hr')
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
