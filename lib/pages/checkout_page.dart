7470a976e053458f055d3a80d6ea46b05fb70472
penhele <stephenhelk@gmail.com>
2024-12-13 12:36:33 +0700

    Header

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -1,6 +1,8 @@
+import 'package:coworkers/config/app_color.dart';
 import 'package:coworkers/controllers/booking_controller.dart';
 import 'package:coworkers/controllers/checkout_controller.dart';
 import 'package:coworkers/controllers/user_controller.dart';
+import 'package:coworkers/widgets/header_worker_left.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -25,7 +27,43 @@ class _CheckoutPageState extends State<CheckoutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: ListView(
+        padding: const EdgeInsets.all(0),
+        children: [
+          SizedBox(
+            height: 172,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: AppColor.bgHeader,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(80),
+                    )
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(0, 60),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      HeaderWorkerLeft(
+                        title: "Checkout",
+                        subtitle: 'Start hiring and grow',
+                        iconLeft: 'assets/ic_back.png',
+                        functionLeft: () {
+                          Navigator.pop(context);
+                        }
+                      ),
+                      // worker(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
