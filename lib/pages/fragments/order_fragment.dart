9c37d9f8ac89ae2363c675a466b8588bbda1aed8
penhele <stephenhelk@gmail.com>
2024-12-13 23:06:05 +0700

    Header

--- lib/pages/fragments/order_fragment.dart
+++ lib/pages/fragments/order_fragment.dart
@@ -1,5 +1,7 @@
+import 'package:coworkers/config/app_color.dart';
 import 'package:coworkers/controllers/fragments/order_controller.dart';
 import 'package:coworkers/controllers/user_controller.dart';
+import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -28,6 +30,83 @@ class _OrderFragmentState extends State<OrderFragment> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return Column(
+      children: [
+        SizedBox(
+          height: 172,
+          child: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: AppColor.bgHeader,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(80),
+                  )
+                ),
+              ),
+              Transform.translate(
+                offset: const Offset(0, 55),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    header(),
+                    DView.height(20),
+                    // menu
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Padding header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          const Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'My Workers',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+                Text(
+                  '13,492 transactions',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            )
+          ),
+          IconButton.filled(
+            onPressed: () {},
+            icon: const ImageIcon(AssetImage('assets/ic_search.png')),
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.white),
+              foregroundColor: WidgetStatePropertyAll(Colors.black)
+            ),
+          ),
+          IconButton.filled(
+            onPressed: () {},
+            icon: const ImageIcon(AssetImage('assets/ic_filter.png')),
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.white),
+              foregroundColor: WidgetStatePropertyAll(Colors.black)
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
