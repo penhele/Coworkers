6968e5e0ffff98633f2a0f2745bab9da01375b86
penhele <stephenhelk@gmail.com>
2024-12-13 12:22:31 +0700

    Checkout Controller

---
+++ lib/controllers/checkout_controller.dart
@@ -0,0 +1,35 @@
+import 'package:coworkers/controllers/booking_controller.dart';
+import 'package:get/get.dart';
+
+class CheckoutController extends GetxController {
+  clear() {
+    Get.delete<CheckoutController>(force: true);
+  }
+
+  final payments = [
+    {
+      'image': 'assets/ic_wallet_payment.png',
+      'label': 'Wallet',
+    },
+    {
+      'image': 'assets/ic_master_card.png',
+      'label': 'CC',
+    },
+    {
+      'image': 'assets/ic_paypal.png',
+      'label': 'PayPal',
+    },
+    {
+      'image': 'assets/ic_other.png',
+      'label': 'Other',
+    },
+  ];
+
+  final _loading = false.obs;
+  bool get loading => _loading.value;
+  set loading(bool n) => _loading.value = n;
+
+  execute() {
+    
+  }
+}
--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -1,4 +1,8 @@
+import 'package:coworkers/controllers/booking_controller.dart';
+import 'package:coworkers/controllers/checkout_controller.dart';
+import 'package:coworkers/controllers/user_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CheckoutPage extends StatefulWidget {
   const CheckoutPage({super.key});
@@ -8,6 +12,16 @@ class CheckoutPage extends StatefulWidget {
 }
 
 class _CheckoutPageState extends State<CheckoutPage> {
+  final checkoutController = Get.put(CheckoutController());
+  final bookingController = Get.put(BookingController());
+  final userController = Get.put(UserController());
+
+  @override
+  void dispose() {
+    checkoutController.clear();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
